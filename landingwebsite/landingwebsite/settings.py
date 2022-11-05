26243d1c465f9a542ec62a5032c975bf5bd6919b
giorgi.soft.group <giorgi.soft.group@gmail.com>
2022-11-06 01:17:29 +0200

    Add templates, CSS and images

--- landingwebsite/landingwebsite/settings.py
+++ landingwebsite/landingwebsite/settings.py
@@ -124,8 +124,8 @@ STATICFILES_DIRS = [
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
