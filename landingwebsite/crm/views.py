5b29f69adc1ff4b9ac2ef99d386c1dd3e3c9ff67
giorgi.soft.group <giorgi.soft.group@gmail.com>
2022-11-09 16:15:04 +0200

    Add several apps

--- landingwebsite/crm/views.py
+++ landingwebsite/crm/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
+from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
+from telebot.sendmessage import sendTelegram
 
 
 def first_page(request):
-    orders = Order.objects.all()
+    slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
     form = OrderForm()
-    return render(request, './index.html', {'orders': orders, 'form': form})
+    dict_obj = {
+        'slider_list': slider_list,
+        'pc_1': pc_1,
+        'pc_2': pc_2,
+        'pc_3': pc_3,
+        'price_table': price_table,
+        'form': form,
+    }
+    return render(request, './index.html', dict_obj)
 
 
 def thanks_page(request):
-    name = request.POST.get('name')
-    phone = request.POST.get('phone')
-    element = Order(order_name = name, order_phone = phone)
-    element.save()
-    return render(request, './thanks-page.html', {"name": name, "phone": phone})
+    if request.POST:
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        element = Order(order_name=name, order_phone=phone)
+        element.save()
+        sendTelegram(tg_name=name, tg_phone=phone)
+        return render(request, './thanks.html', {"name": name, "phone": phone})
+    else:
+        return render(request, './thanks.html')
